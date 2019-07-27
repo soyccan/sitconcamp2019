d969dc61084818f0796d128c2a211910cc6af59c
soyccan <soyccan@soyccan-vm-server>
2019-07-27 17:45:04 +0000

    4 challenges

--- practice/config/initializers/my_constants.rb
+++ practice/config/initializers/my_constants.rb
@@ -1,12 +1,14 @@
 class MyConstants
   TOTAL_TEAMS = 10
 
-  TOTAL_CHALLENGES = 2
+  TOTAL_CHALLENGES = 4
 
   CHALLENGES = {
     # chalid, answer, points
-    1 => ["Taiwan", 10],
-    2 => ["Japan", 20],
+    1 => ["Japan", 10],
+    2 => ["Spain", 15],
+    3 => ["Canada", 20],
+    4 => ["5", 30],
   }
 
   MAX_ROWS = 10
